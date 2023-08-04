2ff5e37aacc4427aeac176b8d8dcd710628fd808
tomasquintana-gj <tomas.quintana@gojiraf.com>
2023-08-04 12:49:52 -0300

    checking later

---
+++ countdown.py
@@ -0,0 +1,7 @@
+def start_timer():
+    reps = 0
+    reps += 1
+
+    work_sec = 1 * 60
+    short_break = 5 * 60
+    long_break_sec = 20 * 60
--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import tkinter as tkinter
 
+def start_timer():
+    reps = 0
+    reps += 1
+
+    work_sec = 25 * 60
+    short_break = 5 * 60
+    long_break = 15 * 60
+
+    count_down(work_sec)
+
+def count_down(time):
+    print(time)
+
+
 window = tkinter.Tk()
 window.title('Feyndoro')
 window.config(padx=100, pady=50, bg="#A5ECFF")
@@ -15,7 +29,6 @@ height_window = 400
 
 x = (width_screen - width_window) // 2
 y = (height_screen - height_window) // 2
-print(x,y)
 
 canvas = tkinter.Canvas(width=350, height=200, highlightthickness=0)
 feynman_img = tkinter.PhotoImage(file='Feynman.png')
@@ -27,5 +40,11 @@ position_center = str((width_screen * height_screen)+ x + y)
 window.geometry(f"540x450+{x}+{y}")
 
 window.attributes('-topmost', 1)
+
+start_button = tkinter.Button(text='Start', command=start_timer, bg='#A5ECFF', highlightthickness=0)
+start_button.grid(column=2, row=2)
+
+reset_button = tkinter.Button(text='Start', command=start_timer, bg='#A5ECFF', highlightthickness=0)
+reset_button.grid(column=2, row=3)
 
 window.mainloop()
